9ee7015c88ca83de1b7d76d5dc45a6e6f64064bc
Eddan99 <edvin.runhellen1@gmail.com>
2026-03-30 15:00:13 +0200

    updated to configdict because of deprecation warning for config

--- app/config.py
+++ app/config.py
@@ -1,12 +1,11 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 class Settings(BaseSettings):
+    model_config = ConfigDict(env_file=".env")
     google_api_key: str
     upload_folder: str = "./uploads"
     embedding_model: str = "models/gemini-embedding-001"
     llm_model: str = "gemini-2.5-flash-lite"
     chroma_path: str = "./chroma_langchain_db"
     collection_name: str = "sherlock_collection"
-
-    class Config:
-        env_file = ".env"
