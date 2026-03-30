6e1e7c7d09f42a3b39fed5c09177849852baf61b
Eddan99 <edvin.runhellen1@gmail.com>
2026-03-30 09:26:06 +0200

    full scripted RAG-workflow implemented

--- ingest.py
+++ ingest.py
@@ -1,13 +1,16 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 
 import getpass
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 file_path = "nke-10k-2023.pdf"
 loader = PyPDFLoader(file_path)
@@ -22,16 +25,7 @@ all_splits = text_splitter.split_documents(docs)
 if not os.environ.get("GOOGLE_API_KEY"):
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
 embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
-
-vector_1 = embeddings.embed_query(all_splits[0].page_content)
-vector_2 = embeddings.embed_query(all_splits[1].page_content)
-
-assert len(vector_1) == len(vector_2)
-print(f"Generated vectors of length {len(vector_1)}\n")
-print(vector_1[:10])
 
 vector_store = Chroma(
     collection_name="example_collection",
@@ -39,10 +33,37 @@ vector_store = Chroma(
     persist_directory="./chroma_langchain_db",
 )
 
-ids = vector_store.add_documents(documents=all_splits)
-
-results = vector_store.similarity_search(
-    "How many distribution centers does Nike have in the US?"
+retriever = vector_store.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3},
 )
 
-print(results[0])
+llm = GoogleGenerativeAI(
+    model="gemini-2.5-flash-lite", 
+    temperature=0.0
+    )
+
+system_prompt = """You are a really good assistant for answering questions based on a document.
+Use only the following context to answer the question.
+If the answer is not in the context, say: "I don't have enough evidence for that."
+
+Context: {context}"""
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    ("human", "{question}"),
+])
+
+chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+question = "how many employees does Nike have?"
+
+print(chain.invoke(question))
