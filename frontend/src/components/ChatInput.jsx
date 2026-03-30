1032894db195daa87a30e250793cb8da327d0c0f
Eddan99 <edvin.runhellen1@gmail.com>
2026-03-30 15:48:38 +0200

    Nicer looking chatinput field, not started with CSS yet

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -1,10 +1,11 @@
 
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 function ChatInput({ onSend, onFileUpload }) {
   const [input, setInput] = useState("")
+  const fileRef = useRef(null)
 
-    function handleSend() {
+  function handleSend() {
     if (!input.trim()) return
     onSend(input)
     setInput("")
@@ -12,9 +13,11 @@ function ChatInput({ onSend, onFileUpload }) {
 
   return (
     <div>
-      <input type="file" accept=".pdf" onChange={onFileUpload} />
+      <input type="file" accept=".pdf" onChange={onFileUpload} ref={fileRef} hidden />
+      <button onClick={() => fileRef.current.click()}>+</button>
       <input
         type="text"
+        placeholder="Ask Sherlock"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
