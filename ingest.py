9d4a45b99007eaf9322ffc1083a6efac6d79f3bd
Eddan99 <edvin.runhellen1@gmail.com>
2026-03-30 08:41:39 +0200

    Added embedding with Google Gemini API

--- ingest.py
+++ ingest.py
@@ -1,8 +1,32 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import getpass
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 file_path = "nke-10k-2023.pdf"
 loader = PyPDFLoader(file_path)
 
 docs = loader.load()
 
-print(len(docs))
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
+
+if not os.environ.get("GOOGLE_API_KEY"):
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+
+vector_1 = embeddings.embed_query(all_splits[0].page_content)
+vector_2 = embeddings.embed_query(all_splits[1].page_content)
+
+assert len(vector_1) == len(vector_2)
+print(f"Generated vectors of length {len(vector_1)}\n")
+print(vector_1[:10])
